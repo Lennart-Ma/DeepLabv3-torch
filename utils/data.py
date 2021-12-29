b8b16ab585c6c60c52b3062b77e6d20296f2ffe2
Lennart <LennartMa.19@gmail.com>
2021-12-29 12:46:43 +0100

    Create dataset class

--- utils/data.py
+++ utils/data.py
@@ -1,10 +1,44 @@
+from numpy.lib.type_check import imag
 import torch
 import os
+import numpy as np
 from typing import Any, Callable, Optional
 
 from torchvision.datasets.vision import VisionDataset
 from PIL import Image
+import torchvision.transforms.functional as TF
+import random
+from torchvision import transforms
 
+
+def apply_transformations(image, mask):
+
+        # image and mask are PIL image object. 
+        img_w, img_h = image.size
+
+        # Random horizontal flipping
+        if random.random() > 0.5:
+                image = TF.hflip(image)
+                mask = TF.hflip(mask)
+
+        # Random vertical flipping
+        if random.random() > 0.5:
+                image = TF.vflip(image)
+                mask = TF.vflip(mask)
+        
+        # Random affine
+        affine_param = transforms.RandomAffine.get_params(
+                degrees = [-180, 180], translate = [0.3,0.3],  
+                img_size = [img_w, img_h], scale_ranges = [1, 1.3], 
+                shears = [2,2])
+        image = TF.affine(image, 
+                        affine_param[0], affine_param[1],
+                        affine_param[2], affine_param[3])
+        mask = TF.affine(mask, 
+                        affine_param[0], affine_param[1],
+                        affine_param[2], affine_param[3])
+
+        return image, mask
 
 
 class SegmentationDataset(VisionDataset):
@@ -13,89 +47,80 @@ class SegmentationDataset(VisionDataset):
     """
 
     def __init__(self,
-                root: str,
-                image_folder: str,
-                mask_folder: str,
-                backend: str = "pil",
-                transforms: Optional[Callable] = None,
+                image_list: list,
+                mask_list: list,
+                train_transforms: bool,
                 seed: int = None,
                 image_color_mode: str = "grayscale",
                 mask_color_mode: str = "grayscale") -> None:
 
         """
         Args:
-            root (str): Root dir
-            image_folder (str): Name of the image folder - located in root dir
-            mask_folder (str): Name of the mask folder - located in root dir
+            image_list: list of paths to the images
+            mask_list: list of paths to the masks
             
             backend: the way the images are loaded - next to pil, one could add cv2
-            transforms (Optional[Callable], optional): A function/transform that takes in
-            a sample and returns a transformed version.
-            E.g, ``transforms.ToTensor`` for images. Defaults to None.
+            train_transforms: boolean to select if transformation from the method transform are applied on dataset (set to None for validation)
 
             seed (int, optional): Specify a seed for the train and test split for reproducible results. Defaults to None.
             image_color_mode (str, optional): 'rgb' or 'grayscale'
             mask_color_mode (str, optional): 'rgb' or 'grayscale'
 
         Data Directory structure:
-            --Train
-            ------Image
+            --train_val
+            ------images
             -----------Image1
             -----------ImageN
-            ------Mask
+            ------masks
             -----------Mask1
             -----------MaskN
-            --Test
-            ------Image
+            --test
+            ------images
             -----------Image1
             -----------ImageN
-            ------Mask
+            ------masks
             -----------Mask1
             -----------MaskN
         """
 
-        super().__init__(root, transforms)
-        image_folder_path = os.path.join(root, image_folder)
-        mask_folder_path = os.path.join(root, mask_folder)
+        super().__init__(transforms)
+        self.backend = "pil"
+        self.train_transforms = train_transforms
+        self.image_list = image_list
+        self.mask_list = mask_list
         self.image_color_mode = image_color_mode
         self.mask_color_mode = mask_color_mode
-
-        self.image_names = sorted(image_folder_path.glob("*"))
-        self.mask_names = sorted(mask_folder_path.glob("*"))
 
         # Raises (OSError, ValueError) could be implemented here
 
     def __len__(self):
-        return len(self.image_folder_path)
+        return len(self.image_list)
 
     def __getitem__(self, item: int):
 
-        image_path = self.image_names[item]
-        mask_path = self.mask_names[item]
+        image = self.image_list[item]
+        mask = self.mask_list[item]
 
-        with open(image_path, 'rb') as image_file, open(mask_path, 'rb') as mask_file:
-
+        with open(image, 'rb') as image_file, open(mask, 'rb') as mask_file:
 
             if self.backend == "pil":
                     
-                image = Image.open(image_file)
+                image = Image.open(image_file).convert("L")
+                
+                mask = Image.open(mask_file).convert("L")
 
-                if self.image_color_mode == "rgb":
-                    image = image.convert("RGB")
-                elif self.image_color_mode == "grayscale":
-                    image = image.convert("L")
-                mask = Image.open(mask_file)
-                if self.mask_color_mode == "rgb":
-                    mask = mask.convert("RGB")
-                elif self.mask_color_mode == "grayscale":
-                    mask = mask.convert("L")
+                normalize = transforms.ToTensor()
 
-                sample = {'image': image, 'mask': mask}
-                if self.transforms:
-                    sample["image"] = self.transforms(sample["image"])
-                    sample["mask"] = self.transforms(sample["mask"])
-            
+                if self.train_transforms:
+                    image, mask = apply_transformations(image, mask)
+
+                image = normalize(image)
+                #mask = normalize(mask)
+                mask = np.array(mask)
+                mask = np.array([mask])
+                mask = torch.tensor(mask)
+                            
             else:
                 raise Exception("Backend not implemented")
 
-            return sample
+            return image, mask
